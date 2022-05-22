0693b6fcc7bd5e0e9231304ab002d9dd5e325477
Michael Smyth <michael.m.smyth@gmail.com>
2022-05-22 14:37:33 +0100

    1st push of template

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import BabylonScene from "./components/BabylonScene.vue";
+</script>
+
+<template>
+  <BabylonScene />
+</template>
+
+<style>
+@import "./assets/reset.css";
+</style>
---
+++ src/components/BabylonScene.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, onMounted } from "@vue/runtime-core";
+import { createScene } from "../scenes/MyFirstScene";
+
+const bjsCanvas = ref(null);
+
+onMounted(() => {
+  if (bjsCanvas.value) {
+    createScene(bjsCanvas.value);
+  }
+});
+</script>
+
+<template>
+  <canvas ref="bjsCanvas" width="500" height="500" />
+</template>
+
+<style></style>
